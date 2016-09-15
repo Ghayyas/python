a3b98e7fd88a566b96ab26b97129d2fc5f3e6290
Ghayyas Mubashir <ghayyasmubashir@gmail.com>
2016-09-15 15:44:45 +0500

    python

---
+++ 2.Modules and Functions/index.py
@@ -0,0 +1,32 @@
+#There is having some modules and functions in Python.
+
+# let say we want to 5 power 4
+
+x = 5 ** 4
+
+print 'The power of 5 to 4 is ' + str(x); 
+
+# an other method to print power is 
+
+x = pow(5,4);
+
+print "The Power of 5 is " + `x`;
+
+# What if we want some more math functions Like Floor? to do this we need to import math library
+
+import math;
+
+x = math.floor(5.904343579);
+
+print "The New Value of X is " + str(x);
+
+
+# Square Root 
+
+x = 2;
+
+z = math.sqrt(x);
+
+print "The value of z and Square root of 2 is " + `z`
+
+
