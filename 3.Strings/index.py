70508dbe324fd8e8e3282eccd27fffb4212608db
Ghayyas Mubashir <ghayyasmubashir@gmail.com>
2016-09-15 16:21:29 +0500

    python

---
+++ 3.Strings/index.py
@@ -0,0 +1,13 @@
+# Lets play with string..
+
+x = 'this is my string';
+
+print x;  # it will print the value of x;
+
+x = "I'm Ghayyas";
+
+print x;
+
+x = "abcd \"is the \" this is";   # this is called ecapse character.
+
+print x;
