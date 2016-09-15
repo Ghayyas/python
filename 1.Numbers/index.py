07d3d2b02c3efeb79ffabee64e4867dbacc3b096
Ghayyas Mubashir <ghayyasmubashir@gmail.com>
2016-09-15 15:01:40 +0500

    python

---
+++ 1.Numbers/index.py
@@ -0,0 +1,45 @@
+# In this example we are going to add simple numbers
+
+2+2;  #it give you 4 but on your console
+
+#To run this follow.
+
+#lets make a simple Variable
+
+# x; #Now x is the variable
+
+x = 2; # we Assign x is 2;
+
+#let print what is the value of x
+
+print "The value of x is " + str(x);   # we type str function because x was the interger and we can not type interger with string 
+
+print "Another value of x is " + `x`;   # the New Shotcut to print x using ecapse character.
+
+x = x+1  #Incrementing
+
+print "Incrementing x value is " + str(x); # now the value of is 3;
+print "Another Incrementing x value " + `x`;
+
+y = 2;
+
+print 'Print Modules of x ' +str(x%y);  #printing Modules of x ? 1 because value of x is 3/2 reminder will be 1
+
+
+#an other method to print modules
+
+#let make it in variable
+
+z = x % y;   # Here we assign value of z which will be equal to reminder of x;
+ 
+# print 'sdfd'
+ 
+print 'The Value of x is ' + str(z);
+ 
+#  #OR
+ 
+print "The value of X is " + `z`;
+
+#Also Please Note Proper indentaion is important in Python to run code.
+
+
--- README.md
+++ README.md
@@ -4,7 +4,27 @@ Simple Working with Python Language.
 
 To run on your terimal type:
 `python index.py` where index.py is any python file name.
+ 
+ Also Please Note Proper Indentation is Very Important in python. Your may not run beacuse of Indentation.
+ 
+ for example
+ `z = y + x`
+   `print z`
+  it will gives you error to indent
+ to fix this you need to properly indent that.
+ 
+ Example:
+ 
+ `z = y + x`
+ `print z`
+ 
+ Now it works fine.
+ 
+ cheers!
 
-#Credits goes to. (Buckey Roberts) and his channel TheNewBoston.
 
-Thankyou Mr.Buckey!
+#Credits goes to (Buckey Roberts) and his channel TheNewBoston.
+
+
+
+Thank you Mr.Buckey!
