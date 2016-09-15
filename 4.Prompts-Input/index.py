bf2d9cc5698bab59e07ac63efbe46f561f11ae52
Ghayyas Mubashir <ghayyasmubashir@gmail.com>
2016-09-15 16:58:18 +0500

    python

---
+++ 4.Prompts-Input/index.py
@@ -0,0 +1,11 @@
+#Lets prompt any number and print it.
+
+x = input('Enter any number: ');
+
+print "The value of x is " + str(x);
+
+# What about if we want to print string the above code will only print numbers if you try to print string it will gives you error.
+
+x = raw_input('Please Enter your name : ');
+
+print "Your Name is : " + x;
---
+++ 5.Sequence OR Array/index.py
@@ -0,0 +1,22 @@
+#if you learn some others language before like Javascript or Java or others.
+#There is a concept of Array which will return a list. but in Python there is no Array but List; both are not preety much same
+
+myArray = [1,2,3,4,5];
+
+print myArray;
+
+#Lets print only one Item in an array
+
+print 'New Item of array is ' + `myArray[3]`;
+
+sequence = [];
+
+number = input (" Please Input any number ");
+
+sequence.append(number);   #Type Append to push
+
+print "Value of sequence is " + `sequence`;  
+
+
+    
+    
